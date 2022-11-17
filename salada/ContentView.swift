0f662e556c5b50d6f3ab76ab37fa1b1af0c0b460
Bruno Casali <brunoocasali@gmail.com>
2022-11-17 00:05:57 -0300

    Initial Commit

---
+++ salada/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  salada
+//
+//  Created by Bruno on 17/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ salada/saladaApp.swift
@@ -0,0 +1,17 @@
+//
+//  saladaApp.swift
+//  salada
+//
+//  Created by Bruno on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct saladaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
